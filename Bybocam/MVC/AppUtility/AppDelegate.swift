9aa08c54cd067b42e2ddd524ab19b5258cc6bfd4
Your Real NameHardeep <firebase618@gmail.com>
2020-06-19 13:40:59 +0530

    pod

--- Bybocam/MVC/AppUtility/AppDelegate.swift
+++ Bybocam/MVC/AppUtility/AppDelegate.swift
@@ -28,7 +28,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate,UNUserNotificationCenterDe
         BioMetricAuthenticator.shared.allowableReuseDuration = 60
          registerForRemoteNotification()
         
-        //Git update code
+        
         return true
     }
     func Autologin()
